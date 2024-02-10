f4f393e622fbcb53bc29bf485b513f5dc75445d0
Vocal <vocalnight@protonmail.com>
2024-02-09 21:01:12 -0300

    project finished

---
+++ React.ExternalAPI.VocalNight/api-proj/src/App.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import MainPage from './Pages/MainPage';
+
+test('renders screen', () => {
+  render(<App />);
+
+});
+
+test('renders screen games', () => {
+  render(<MainPage />);
+
+});
+
+test('renders screen games', () => {
+  render(<MainPage />);
+
+});
---
+++ React.ExternalAPI.VocalNight/api-proj/src/Pages/NotFoundPage.js
@@ -0,0 +1,5 @@
+export default function NotFoundPage() {
+    return(
+        <h1>Ops. This page was not found!</h1>
+    )
+}
