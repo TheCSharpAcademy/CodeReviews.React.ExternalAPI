d3774110c21de0862455e67a8b0a958c7f3912dc
Stan Choi <sjc591@nyu.edu>
2024-03-01 18:22:42 -0500

    Naively add 'Loading Data...' while request to list hitters is still in progress

--- React.ExternalAPI/react.externalapi.client/src/App.jsx
+++ React.ExternalAPI/react.externalapi.client/src/App.jsx
@@ -3,12 +3,20 @@ import './App.css';
 
 function App() {
     const [hitters, setHitters] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
         fetch('https://localhost:7286/api/hitter/')
             .then(response => response.json())
-            .then(data => setHitters(data));
+            .then(data => {
+                setHitters(data);
+                setIsLoading(false);
+            });
     }, []);
 
+    if (isLoading) {
+        return <p>Loading Data...</p>
+    }
     if (hitters.length) {
         console.log(hitters[0])
         return <img src={hitters[0].imageUrl} alt="juan soto"></img>
