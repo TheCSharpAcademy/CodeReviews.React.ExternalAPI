ee6154d026adfe9cdd17b644cfdf0f310c551a59
Stan Choi <sjc591@nyu.edu>
2024-03-01 19:57:41 -0500

    Finish first pass

--- React.ExternalAPI/react.externalapi.client/src/App.jsx
+++ React.ExternalAPI/react.externalapi.client/src/App.jsx
@@ -1,36 +1,15 @@
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Hitters from './components/Hitters';
+import HitterDetail from './components/HitterDetail';
 import './App.css';
 
 function App() {
-    const [hitters, setHitters] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        fetch('https://localhost:7286/api/hitter/')
-            .then(response => response.json())
-            .then(data => {
-                setHitters(data);
-                setIsLoading(false);
-            });
-    }, []);
-
-    if (isLoading) {
-        return <p>Loading Data...</p>
-    }
-
     return (
         <Router>
-            <>
-                <h1>List of Best 2024 Baseball Hitters based on 2023 Stats</h1>
-                <ul>
-                    {hitters.map(hitter => {
-                        <li key={hitter.Id}>
-                            <Link to=`/api/hitters/${hitter.Id}`>{hitter.Name}</Link>
-                        </li>
-                    })}
-                </ul>
-            </>
+            <Routes>
+                <Route path="/" element={<Hitters />} />
+                <Route path="/hitters/:id" element={<HitterDetail />} />
+            </Routes>
         </Router>
     )
 }
