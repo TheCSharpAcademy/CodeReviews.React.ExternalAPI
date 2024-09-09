f341383e1768dac0bfadabaa18137c15b8b4bb64
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2024-09-09 20:36:17 +0100

    Finished project

---
+++ ExternalApi.chrisjamiecarter/src/Wizards.Api/Services/ISeederService.cs
@@ -0,0 +1,9 @@
+﻿namespace Wizards.Api.Services;
+
+/// <summary>
+/// Defines the contract for a service that manages Seeding a Database with entities.
+/// </summary>
+internal interface ISeederService
+{
+    void SeedDatabase();
+}
